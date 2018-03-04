db16608d30eaa78b6f2389b55a434f38f4fd0c5b
M1LKYWVY <ruslan.filichkin@iCloud.com>
2018-03-04 20:07:06 +0300

    License added. Unused folders removed

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import time
 
 
+def main():
+    for i in range(1000):
+        print(str(i) + " attempt to proove that Liza is the best girl in the world!")
+        time.sleep(3)
+
+
+if __name__ == "__main__":
+    main()
