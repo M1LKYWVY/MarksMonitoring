c01cf428ffe530ad9db8e1dd5bc1ab8a2ff96547
M1LKYWVY <ruslan.filichkin@iCloud.com>
2018-03-09 11:55:32 +0300

    Code refactoring

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ import time
 
 
 def main():
-    for i in range(1000):
-        print(str(i) + " attempt to proove that Liza is the best girl in the world!")
-        time.sleep(3)
+    
 
-
-if __name__ == "__main__":
+if __main__ == "__main__":
     main()
